83f16090e99d6bd7e0f7e239a7f955754de1c55d
lbytyci <74509833+lbytyci@users.noreply.github.com>
2021-07-23 15:50:12 +0200

    Create DigitalSignatureResult.cs

---
+++ ClientSide/DigitalSignatureResult.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientSide
+{
+   
+	public class DigitalSignatureResult
+	{
+		public string CipherText { get; set; }
+		public string SignatureText { get; set; }
+	}
+}
